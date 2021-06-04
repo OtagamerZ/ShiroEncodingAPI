bf769c7b5e37861dcce093102632802630d1bb11
ygimenez <yagogimenez1@hotmail.com>
2021-06-04 11:36:19 -0300

    Update

--- src/main/java/api/handler/EncodingQueue.java
+++ src/main/java/api/handler/EncodingQueue.java
@@ -9,14 +9,14 @@ import java.io.IOException;
 public class EncodingQueue {
 	public void queue(VideoData data) {
 		if (!data.isComplete()) {
-			Application.sockets.getEncoder().send(new JSONObject(){{
+			Application.sockets.getEncoder().getClient().send(new JSONObject(){{
 					put("code", HttpStatus.EXPECTATION_FAILED.value());
 					put("message", "Invalid frame size: received " + data.getFrames().size() + " | expected " + data.getSize());
 			}}.toString());
 			return;
 		}
 
-		Application.getExec().execute(() -> Application.sockets.getEncoder().send(new JSONObject(){{
+		Application.getExec().execute(() -> Application.sockets.getEncoder().getClient().send(new JSONObject(){{
 			put("code", HttpStatus.OK.value());
 			put("hash", data.getHash());
 			try {
