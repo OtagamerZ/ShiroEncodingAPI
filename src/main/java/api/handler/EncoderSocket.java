ed12dfba08db6f2e6dcbc543ec0c1b29ea5375a1
ygimenez <yagogimenez1@hotmail.com>
2021-06-05 00:03:28 -0300

    Update

--- src/main/java/api/handler/EncoderSocket.java
+++ src/main/java/api/handler/EncoderSocket.java
@@ -21,6 +21,7 @@ package api.handler;
 import api.Application;
 import org.apache.commons.lang3.ArrayUtils;
 import org.java_websocket.WebSocket;
+import org.java_websocket.framing.CloseFrame;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 import org.json.JSONException;
@@ -43,14 +44,14 @@ public class EncoderSocket extends WebSocketServer {
 
 	@Override
 	public void onOpen(WebSocket conn, ClientHandshake handshake) {
-		/*if (!handshake.getFieldValue("Authentication").equals(System.getenv("AUTH"))) {
+		if (!handshake.getFieldValue("Authentication").equals(System.getenv("AUTH"))) {
 			conn.send(new JSONObject() {{
 				put("code", HttpStatus.UNAUTHORIZED.value());
 				put("message", "Connection not authorized for supplied token");
 			}}.toString());
 			conn.close(CloseFrame.REFUSE);
 			return;
-		}*/
+		}
 
 		Application.logger.debug("Connection estabilished: " + conn.getRemoteSocketAddress().toString());
 	}
